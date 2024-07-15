f15cc060ea128e8c348cdf66bd553a28cae04552
PhGirish <girizph8@gmail.com>
2024-07-16 00:13:28 +0530

    1st day done

---
+++ day1/day1.js
@@ -0,0 +1,28 @@
+//Task 1
+
+var number = 1;
+let string = "girish";
+
+console.log(number);
+console.log(string);
+
+//Task 2
+const bool = true;
+console.log(bool);
+
+//Task 3
+console.log(typeof bool);
+console.log(typeof number);
+console.log(typeof string);
+
+//Task 4
+let re = 6;
+re = 0;
+
+console.log(re);
+
+//Task 5
+bool = false;
+
+//FR
+console.log(number + " " + typeof number);
